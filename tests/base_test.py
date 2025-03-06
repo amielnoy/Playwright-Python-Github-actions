1b185cd9c415ddbab04c58e40d34cdff7fe23055
amielpeled <amielnoy@gmail.com>
2025-03-06 22:24:12 +0200

    add BaseTest with static method

---
+++ tests/base_test.py
@@ -0,0 +1,17 @@
+from geventhttpclient import requests
+
+from utilities.constants import Constants
+
+class BaseTest():
+    @staticmethod
+    def get_public_ip() -> str:
+        """Function to retrieve public IP address.
+
+        Returns:
+            str: Public IP address.
+        """
+        return requests.get(
+            "http://checkip.amazonaws.com",
+            timeout=40,
+            headers={"User-Agent": Constants.AUTOMATION_USER_AGENT},
+        ).text.rstrip()
--- tests/checkout_test.py
+++ tests/checkout_test.py
@@ -2,9 +2,10 @@ import pytest
 from playwright.sync_api import Page, expect
 
 from enums.User import User
+from tests.base_test import BaseTest
 
 
-class TestCheckout:
+class TestCheckout(BaseTest):
 
     @pytest.mark.parametrize(
         "browser_context_args", [User.STANDARD_USER], indirect=True
@@ -12,4 +13,7 @@ class TestCheckout:
     def test_checkout_counter(self, browser_context_args, page: Page):
         page.evaluate("localStorage.setItem('cart-contents', '[4,0]');")
         page.reload()
+        actual_site_ip=BaseTest.get_public_ip()
+        expected_site_ip=""
+        expect(actual_site_ip).to_have_text(expected_site_ip)
         expect(page.get_by_test_id("shopping-cart-badge")).to_have_text("2")
