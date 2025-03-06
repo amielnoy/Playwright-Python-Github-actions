ff1c00505d49e6d25c0a1260000d0e52cc11e845
amielpeled <amielnoy@gmail.com>
2025-03-06 21:09:58 +0200

    --Add nondestructive pytest marker to allow test run --Add new test with additional allowed serious violation

--- tests/accesability_test.py
+++ tests/accesability_test.py
@@ -1,4 +1,5 @@
 import allure
+import pytest
 
 
 class TestAccessibility:
@@ -7,6 +8,7 @@ class TestAccessibility:
     def test_accessibility_default_counts(self, axe_playwright, page):
         axe_playwright.check_accessibility(page)
 
+    @pytest.mark.nondestructive
     @allure.title("Test Accessibility with Custom Counts")
     def test_accessibility_custom_counts(self, axe_playwright, page):
         axe_playwright.check_accessibility(
@@ -15,6 +17,18 @@ class TestAccessibility:
                 "minor": 2,
                 "moderate": 5,
                 "serious": 0,
+                "critical": 0,
+            },
+        )
+    @pytest.mark.nondestructive
+    @allure.title("Test Accessibility with Custom Counts")
+    def test_accessibility_custom_counts(self, axe_playwright, page):
+        axe_playwright.check_accessibility(
+            page,
+            maximum_allowed_violations_by_impact={
+                "minor": 5,
+                "moderate": 5,
+                "serious": 1,
                 "critical": 0,
             },
         )
